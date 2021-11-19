15466750493502a705e24ab2bd1b53696de6b7fb
Zioyi <newsunstudy@163.com>
2021-11-19 22:58:15 +0800

    communicate by pipe

--- container/init.go
+++ container/init.go
@@ -1,27 +1,46 @@
 package container
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"strings"
 	"syscall"
 
-	"github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 )
 
-func RunContainerInitProcess(cmdArray []string, args []string) error {
+func RunContainerInitProcess() error {
 	// not mount, it works
 	// defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
 	// syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
+	cmdArray := readUserCommand()
+	if len(cmdArray) == 0 {
+		return fmt.Errorf("run container get user command error, cmdArray is nil")
+	}
 
 	path, err := exec.LookPath(cmdArray[0])
 	if err != nil {
-		logrus.Warnf("Not found path, %v", cmdArray[0])
+		log.Warnf("Not found path, %v", cmdArray[0])
 		return err
 	}
 
-	logrus.Infof("Find path %s", path)
+	log.Infof("Find path %s", path)
 	if err := syscall.Exec(path, cmdArray[0:], os.Environ()); err != nil {
-		logrus.Errorf(err.Error())
+		log.Errorf(err.Error())
 	}
 	return nil
+}
+
+func readUserCommand() []string {
+	// uintptr(3) 就是指 index 为 3 的文件描述符，也就是传递进来的管道一端
+	pipe := os.NewFile(uintptr(3), "pipe")
+	msg, err := ioutil.ReadAll(pipe)
+	if err != nil {
+		log.Errorf("init read pipe error %v", err)
+		return nil
+	}
+	msgStr := string(msg)
+	return strings.Split(msgStr, " ")
 }
