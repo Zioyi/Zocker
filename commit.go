e9ccf95ac55c833552c9eca729fed120a174eb64
Zioyi <newsunstudy@163.com>
2022-01-01 22:40:13 +0800

    fix compress cmd args

--- commit.go
+++ commit.go
@@ -11,7 +11,7 @@ func commitContainer(imageName string) {
 	mntURL := "/root/mnt/"
 	imageTar := "/root/" + imageName + ".tar"
 	fmt.Printf("%s", imageTar)
-	if _, err := exec.Command("tar", "-czf", imageTar, "-c", mntURL, ".").CombinedOutput(); err != nil {
+	if _, err := exec.Command("tar", "-czf", imageTar, "-C", mntURL, ".").CombinedOutput(); err != nil {
 		log.Errorf("tar folder %s error %v", mntURL, err)
 	}
 }
