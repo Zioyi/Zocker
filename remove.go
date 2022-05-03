b47169695ff890433c92cad4693945c0d69e46e5
Zioyi <newsunstudy@163.com>
2022-05-03 22:00:12 +0800

    impl remove a container

---
+++ remove.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/Zioyi/zocker/container"
+)
+
+func RemoveContainer(containerName string) {
+	containerInfo, err := getContainerInfoByName(containerName)
+	if err != nil {
+		log.Errorf("[RemoveContainer] get container info by name [%s] failed, err = %v", containerName, err)
+		return
+	}
+
+	if containerInfo.Status != container.Stop {
+		log.Errorf("[RemoveContainer] container status [%s] is not stopped", containerInfo.Status)
+		return
+	}
+
+	containerDir := fmt.Sprintf(container.DefaultInfoLocation, containerName)
+	if err := os.RemoveAll(containerDir); err != nil {
+		log.Errorf("[RemoveContainer] remove container dir [%s] failed, err = %v", containerDir, err)
+		return
+	}
+}
